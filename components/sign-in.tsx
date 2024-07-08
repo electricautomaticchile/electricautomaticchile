a864e7811204391e95b6a6264d8c43ca74bcacbc
pipeaalzamora <pipeaalzamora@gmail.com>
2024-07-08 17:53:09 -0400

    auth google

---
+++ components/sign-in.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./ui/button"
+import { signIn } from "@/auth"
+ 
+export function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server"
+        await signIn("google")
+      }}
+    >
+      <Button  className="w-full" type="submit">Iniciar Sesion con Google</Button>
+    </form>
+  )
+} 
