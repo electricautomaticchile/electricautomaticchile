60eab312e05c0c67fc491b1b9bc6f960d19c5cef
pipeaalzamora <pipeaalzamora@gmail.com>
2024-07-08 18:36:56 -0400

    fix dark-theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
       <ThemeProvider
             attribute="class"
             defaultTheme="blanco"
-            enableSystem
             themes={["blanco","oscuro"]}
             disableTransitionOnChange
           >
