2d6439b928d0d9b7e3a0fe654e9382ca7065e781
pipeaalzamora <pipeaalzamora@gmail.com>
2024-07-24 15:08:15 -0400

    add auth amplify

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -6,7 +6,7 @@ export function SignIn() {
     <form
       action={async () => {
         "use server"
-        await signIn("google")
+        await signIn("google", { redirectTo: "/dashboard" })
       }}
     >
       <Button  className="w-full" type="submit">Iniciar Sesion con Google</Button>
