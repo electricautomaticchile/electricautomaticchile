21ad5e549e6ad553d49ad13b8f7b413a2a8eda45
pipeaalzamora <pipeaalzamora@gmail.com>
2024-12-15 22:16:52 -0300

    Se muestran todo los graficos al linkear graficos de energia

--- app/dashboard/components-dashboard/nav-dash/nav-dash.tsx
+++ app/dashboard/components-dashboard/nav-dash/nav-dash.tsx
@@ -45,7 +45,7 @@ export default function Navbar({ onComponentClick }: NavbarProps) {
           <div className={`flex-1 overflow-auto py-2 ${isMenuOpen ? 'block' : 'hidden'} lg:block`}>
             <nav className="grid items-start px-4 text-l font-medium">
               <NavButton icon={<Home className="h-4 w-4" />} text="Inicio" onClick={() => onComponentClick(null)} />
-              <NavButton icon={<Bolt className="h-4 w-4" />} text="Control de consumo" onClick={() => onComponentClick("EnergyMounth")} />
+              <NavButton icon={<Bolt className="h-4 w-4" />} text="Graficos de energia" onClick={() => onComponentClick("EnergyUsedMonth")} />
               <NavButton icon={<Battery className="h-4 w-4" />} text="Corte y reposición" onClick={() => onComponentClick("ReposicionPage")} />
               <NavButton icon={<DollarSign className="h-4 w-4" />} text="Pagos" onClick={() => onComponentClick("Payments")} />
               <NavButton icon={<Map className="h-4 w-4" />} text="Sectores sin Energía" onClick={() => onComponentClick("SectorEnergy")} />
