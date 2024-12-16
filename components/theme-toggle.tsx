147d492ab147f62207554b2434bd8c61cb9ad204
pipeaalzamora <pipeaalzamora@gmail.com>
2024-12-15 22:20:14 -0300

    Se modifico de manera correcta el dark-mode

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,9 +1,7 @@
 "use client"
-
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -13,23 +11,33 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ThemeToggle() {
-  const { setTheme, themes } = useTheme()
+  const { setTheme, theme } = useTheme()
+  const availableThemes = ["blanco", "oscuro"]
+
+  React.useEffect(() => {
+    if (!theme) {
+      setTheme("blanco")
+    }
+  }, [theme, setTheme])
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          {theme === "oscuro" ? (
+            <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+          ) : (
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all" />
+          )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        {[themes.map(theme =>
+        {availableThemes.map(theme =>
           <DropdownMenuItem key={theme} onClick={() => setTheme(theme)}>
             {theme}
-        </DropdownMenuItem>
-        )]}
+          </DropdownMenuItem>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   )
