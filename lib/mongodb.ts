c05de7f79bfba03db4adaa635fc97adb02b9da95
pipeaalzamora <pipeaalzamora@gmail.com>
2024-12-26 13:45:42 -0300

    fix 4

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,25 +1,34 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI;
+import { MongoClient, ServerApiVersion } from "mongodb"
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+if (!process.env.MONGODB_URI) {
+  throw new Error('Falta la variable de entorno MONGODB_URI')
+}
+
+const uri = process.env.MONGODB_URI
+const options = {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
 }
-run().catch(console.dir);
+
+let client
+let clientPromise: Promise<MongoClient>
+
+if (process.env.NODE_ENV === "development") {
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>
+  }
+
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri, options)
+    globalWithMongo._mongoClientPromise = client.connect()
+  }
+  clientPromise = globalWithMongo._mongoClientPromise
+} else {
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
+}
+
+export default clientPromise 
