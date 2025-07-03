4bca2e58087c210b2ab7626e59b1abb34634e23b
pipeaalzamora <pipeaalzamora@gmail.com>
2025-07-02 22:19:40 -0400

    ✨ Añadir archivo de exportación de hooks - Se crea un nuevo archivo para exportar hooks relacionados con la gestión de clientes y se deja espacio para futuras exportaciones de otros hooks, mejorando la organización y modularidad del código.

---
+++ lib/hooks/queries/index.ts
@@ -0,0 +1,11 @@
+// Exportar hooks de clientes
+export * from "./useClientesQuery";
+
+// Exportar hooks de cotizaciones (cuando estén listos)
+// export * from "./useCotizacionesQuery";
+
+// Aquí se pueden agregar más hooks cuando se refactoricen otros componentes
+// Por ejemplo:
+// export * from "./useUsuariosQuery";
+// export * from "./useEmpresasQuery";
+// export * from "./useDispositivosQuery";
