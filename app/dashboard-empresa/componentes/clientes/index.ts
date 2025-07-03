f5b06dd4ad8ca4041574a070a424e4c9b7be0e0d
pipeaalzamora <pipeaalzamora@gmail.com>
2025-07-03 12:31:31 -0400

    ✨ Eliminar componentes de clientes - Se eliminan archivos y lógica relacionados con los componentes de clientes, incluyendo subcomponentes, tipos y hooks, para simplificar la estructura del código y mejorar la mantenibilidad. Se mantiene la funcionalidad existente en otros módulos.

--- app/dashboard-empresa/componentes/clientes/index.ts
+++
@@ -1,9 +0,0 @@
-// Exportar todos los componentes de clientes
-export { ClienteModal } from "./ClienteModal";
-export { ClientesFiltros } from "./ClientesFiltros";
-export { ClientesAcciones } from "./ClientesAcciones";
-export { ClientesEstadisticas } from "./ClientesEstadisticas";
-export { ClientesTabla } from "./ClientesTabla";
-
-// Exportar tipos si es necesario
-export type { EstadisticasData } from "./ClientesEstadisticas";
