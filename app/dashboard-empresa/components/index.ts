acb72c39074de7fa9bdeb782583463620ae55ce4
pipeaalzamora <pipeaalzamora@gmail.com>
2025-07-03 17:30:53 -0400

    ✨ Establecer consistencia en las comillas en index.ts - Se actualizan las comillas simples a comillas dobles en las exportaciones de componentes y características, mejorando la uniformidad del código en el proyecto.

--- app/dashboard-empresa/components/index.ts
+++ app/dashboard-empresa/components/index.ts
@@ -1,16 +1,16 @@
 // Componentes de Layout
-export { EncabezadoEmpresa as Header } from './layout/header';
-export { EncabezadoEmpresa } from './layout/header';
-export { BarraNavegacionLateral as Navigation } from './layout/navigation';
-export { BarraNavegacionLateral } from './layout/navigation';
+export { EncabezadoEmpresa as Header } from "./layout/header";
+export { EncabezadoEmpresa } from "./layout/header";
+export { BarraNavegacionLateral as Navigation } from "./layout/navigation";
+export { BarraNavegacionLateral } from "./layout/navigation";
 
 // Re-exportar features para mantener compatibilidad
-export { AlertasSistema } from '../features/alertas';
-export { DispositivosActivos } from '../features/dispositivos';
-export { EstadisticasConsumo } from '../features/estadisticas';
-export { ConfiguracionEmpresa } from '../features/configuracion';
-export { ConsumoSectorial } from '../features/consumo';
-export { ControlArduino } from '../features/control';
+export { AlertasSistema } from "../features/alertas";
+export { DispositivosActivos } from "../features/dispositivos";
+export { EstadisticasConsumo } from "../features/estadisticas";
+export { ConfiguracionEmpresa } from "../features/configuracion";
+export { ConsumoSectorial } from "../features/consumo";
+export { ControlArduino } from "../features/control";
 
 // Exportar componentes de clientes
-export * from '../features/clientes';
+export * from "../features/clientes";
